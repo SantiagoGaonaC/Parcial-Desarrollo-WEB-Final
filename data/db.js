63f6f819753b92809cc2d20ba87a33a243ce56c1
vudumstead <santiago.gaona.2018@upb.edu.co>
2022-11-02 22:13:46 -0500

    Creación del SCHEMA de DB

--- data/db.js
+++ data/db.js
@@ -11,3 +11,39 @@ let db = new sqlite3.Database(
     console.log("Conectado a la base de datos. sqlite3");
   }
 );
+
+const sql_usuarios = `CREATE TABLE IF NOT EXISTS usuarios (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  email TEXT NOT NULL UNIQUE,
+  nombre TEXT NOT NULL,
+  apellido TEXT NOT NULL,
+  contraseña TEXT NOT NULL,
+  favoritos INTEGER
+);`
+
+const sql_pedidos = `
+CREATE TABLE IF NOT EXISTS pedidos (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  comprador INTEGER REFERENCES usuarios (id),
+  products TEXT NOT NULL,
+  subtotal FLOAT NOT NULL,
+  total FLOAT NOT NULL
+);`
+
+
+db.serialize(() => {
+  db.run(sql_usuarios,error=>{
+    if(error){
+      return console.error(error.message);
+    }else{
+      console.log("Tabla USUARIOS creada correctamente")
+    }
+  });
+  db.run(sql_pedidos,error=>{
+    if(error){
+      return console.error(error.message);
+    }else{
+      console.log("Tabla PEDIDOS creada correctamente")
+    }
+  });
+});
