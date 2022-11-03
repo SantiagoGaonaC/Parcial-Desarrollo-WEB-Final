42a08b62a8bda614fe9dd7e6369aed1f1dff5ffb
vudumstead <santiago.gaona.2018@upb.edu.co>
2022-11-03 17:22:39 -0500

    boton<a> añadir a la cesta

--- routers/carrito.js
+++ routers/carrito.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const carritoController = require("../controllers/carritoController");
 
 router.get("/carrito", carritoController.mostrar);
-router.get("/carrito/:id", carritoController.añadirCarrito);
-router.get("/carrito/:id", carritoController.borrar);
-router.get("/carrito/:id", carritoController.actualizar);
+router.get("/agregar-carrito/:id", carritoController.añadirCarrito);
+router.delete("/carrito/:id", carritoController.borrar);
+router.put("/carrito/:id", carritoController.actualizar);
 module.exports = router;
