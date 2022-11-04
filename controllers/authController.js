a166674a0eef884bd746174ad025bd2062a7fab3
vudumstead <santiago.gaona.2018@upb.edu.co>
2022-11-04 16:35:40 -0500

    authcontroller register

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,45 +7,6 @@ module.exports.mostrar = (req, res) => {
   //res.status(200).json(carrito);
 };
 
-/* exports.register = async (req, res) => {
-  email_ex =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  try {
-    const nombre = req.body.nombre;
-    const apellido = req.body.apellido;
-    const email = req.body.email;
-    const salt = await bcryptjs.genSalt(10);
-    const passHash = await bcryptjs.hash(req.body.password, salt);
-
-    db.run(
-      "INSERT INTO usuarios(nombre, apellido, email, contraseña) VALUES(?, ?, ?, ?)",
-      [nombre, apellido, email, passHash],
-      (error, results) => {
-        if (error) {
-          if (
-            error.code == "ER_DUP_ENTRY" ||
-            error.errno == 1062 ||
-            error.errno == 19
-          ) {
-            console.log("Usuario duplicado");
-            res.redirect("/");
-          } else {
-            console.log("Otro error en la query");
-            console.log(error);
-            res.redirect("/");
-          }
-        } else {
-            console.log("Usuario registrado");
-            //res.redirect('/',NotifySweetAlert.RegistroExitoso())
-            res.redirect("/");
-        }
-      }
-    );
-  } catch (error) {
-    console.log(error);
-  }
-}; */
-
 exports.register = async (req, res) => {
   email_ex =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
